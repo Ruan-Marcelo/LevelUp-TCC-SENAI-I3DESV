1e416cc5e4573de549697cbd0e7caabbcea1fd09
ZruanZito <blackingody@gmail.com>
2025-09-29 11:19:09 -0300

    Produto

---
+++ LEVELUP/LevelUp/LevelUp/Admin/Produto.aspx
@@ -0,0 +1,69 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/Admin.Master" AutoEventWireup="true" CodeBehind="Produto.aspx.cs" Inherits="LevelUp.Admin.Produto" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <div class="mb-4">
+        <asp:Label ID="lblMsg" runat="server" CssClass="h3"></asp:Label>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12 col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">Categoria</h4>
+                    <hr />
+
+                    <div class="form-body">
+
+                        <label>Categoria Nome:</label>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <asp:TextBox ID="txtCategoriaNome" runat="server" CssClass="form-control" placeholder="Digite o nome da categoria"></asp:TextBox>
+                                    <asp:RequiredFieldValidator ID="rfvCategoriaNome" runat="server" ForeColor="Red" FontSize="Small"
+                                        Display="Dynamic" SetFocusOnError="True" ControlToValidate="txtCategoriaNome"
+                                        ErrorMessage="O nome da categoria é obrigatório." CssClass="text-danger">
+                                    </asp:RequiredFieldValidator>
+                                </div>
+                            </div>
+                        </div>
+
+                        <label>Categoria Image:</label>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <asp:FileUpload ID="fuCategoriaImagem" runat="server" CssClass="form-control"
+                                        onchange="ImagemPreview(this);" />
+                                    <asp:HiddenField ID="hfCategoriaId" runat="server" Value="0" />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <asp:CheckBox ID="cbIsActive" runat="server" Text="&nbsp;  Está Ativo" />
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="form-adction pb-5">
+                        <div class="text-left">
+                            <asp:Button ID="btnAddOrUpdate" runat="server" CssClass="btn btn-info" Text="Adicionar" OnClick="btnAddOrUpdate_Click" />
+                            <asp:Button ID="btnClear" runat="server" CssClass="btn btn-dark" Text="Limpar" OnClick="btnClear_Click" />
+                        </div>
+                    </div>
+
+                    <div>
+                        <asp:Image ID="imagemPreview" runat="server" CssClass="img-thumbail" AlternateText="" />
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ LEVELUP/LevelUp/LevelUp/Admin/Produto.aspx.designer.cs
@@ -0,0 +1,98 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Esse código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace LevelUp.Admin
+{
+
+
+    public partial class Produto
+    {
+
+        /// <summary>
+        /// Controle lblMsg.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMsg;
+
+        /// <summary>
+        /// Controle txtCategoriaNome.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCategoriaNome;
+
+        /// <summary>
+        /// Controle rfvCategoriaNome.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvCategoriaNome;
+
+        /// <summary>
+        /// Controle fuCategoriaImagem.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FileUpload fuCategoriaImagem;
+
+        /// <summary>
+        /// Controle hfCategoriaId.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField hfCategoriaId;
+
+        /// <summary>
+        /// Controle cbIsActive.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox cbIsActive;
+
+        /// <summary>
+        /// Controle btnAddOrUpdate.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAddOrUpdate;
+
+        /// <summary>
+        /// Controle btnClear.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnClear;
+
+        /// <summary>
+        /// Controle imagemPreview.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image imagemPreview;
+    }
+}
